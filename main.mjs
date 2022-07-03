18034f99bc13e920380d4324216c3d72d220238f
Ericson Carlos <you@example.com>
2022-07-02 21:06:30 -0300

    primeiro commit

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log (mostraIdade (`Ericson`, 39));
+console.log(mostraCidade(`Ericson`, `Paulista`));
+console.log(mostraHobby(`Ericson`, `tocar teclado`));
